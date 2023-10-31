f6926ad450e10b1da9d3f248a7bf04c81e19fae2
Ilija Lazoroski <ilija.la@live.com>
2023-10-31 14:40:14 +0000

    Import AgentID from monkeytypes
    Original commit https://github.com/guardicore/monkey/commit/9a7454cdb7cc1600a75620b090baa7642d95b474

--- tests/test_ransomware.py
+++ tests/test_ransomware.py
@@ -15,7 +15,7 @@ from ransomware_payload.typedef import (
     FileSelectorCallable,
     ReadmeDropperCallable,
 )
-from monkeytypes import Event
+from monkeytypes import AgentID, Event
 from tests.ransomware_target_files import (
     ALL_ZEROS_PDF,
     HELLO_TXT,
@@ -25,7 +25,6 @@ from tests.utils import is_user_admin
 
 from common.agent_events import AbstractAgentEvent, FileEncryptionEvent
 from common.event_queue import AgentEventSubscriber, IAgentEventPublisher
-from common.types import AgentID
 
 BuildRansomwareCallable: TypeAlias = Callable[
     [InternalRansomwareOptions, FileEncryptorCallable, FileSelectorCallable, ReadmeDropperCallable],
