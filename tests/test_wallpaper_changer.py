8b7245f32c11719828a67a76518c8f3cfd889150
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-11-09 09:32:32 -0500

    Add unit tests for ransomware's WallpaperChanger
    Original commit https://github.com/guardicore/monkey/commit/518ee4fa4f8c8621f63a43cac3ca6ab3776b5344

---
+++ tests/test_wallpaper_changer.py
@@ -0,0 +1,41 @@
+from unittest.mock import MagicMock
+
+import pytest
+from ransomware_payload.wallpaper_changer import WallpaperChanger
+from monkeytypes import OperatingSystem
+
+
+@pytest.fixture
+def SystemParametersInfoW() -> MagicMock:
+    return MagicMock()
+
+
+@pytest.fixture
+def ctypes(SystemParametersInfoW: MagicMock) -> MagicMock:
+    ctypes = MagicMock()
+    ctypes.windll = MagicMock()
+    ctypes.windll.user32 = MagicMock()
+    ctypes.windll.user32.SystemParametersInfoW = SystemParametersInfoW
+
+    return ctypes
+
+
+@pytest.fixture(autouse=True)
+def patch_ctypes(monkeypatch, ctypes):
+    monkeypatch.setattr("ransomware_payload.wallpaper_changer.ctypes", ctypes)
+
+
+def test_windows(SystemParametersInfoW: MagicMock):
+    wallpaper_changer = WallpaperChanger(OperatingSystem.WINDOWS)
+
+    wallpaper_changer.change_wallpaper()
+
+    assert SystemParametersInfoW.call_count == 1
+
+
+def test_not_windows(SystemParametersInfoW: MagicMock):
+    wallpaper_changer = WallpaperChanger(OperatingSystem.LINUX)
+
+    wallpaper_changer.change_wallpaper()
+
+    assert SystemParametersInfoW.call_count == 0
