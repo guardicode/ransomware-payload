c852605b63b7abb01295a6712db43ed92fe14de4
Ilija Lazoroski <ilija.la@live.com>
2024-07-08 14:17:34 +0000

    Add data_for_tests_dir in conftests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,23 @@ from pathlib import Path
 
 import pytest
 
+from monkeytoolbox import get_os
+from monkeytypes import OperatingSystem
+
+
+@pytest.fixture(scope="session")
+def data_for_tests_dir(pytestconfig) -> Path:
+    return Path(pytestconfig.rootdir) / "tests" / "data_for_tests"
+
+
+@pytest.fixture(scope="session")
+def stable_file(data_for_tests_dir: Path) -> Path:
+    return data_for_tests_dir / "stable_file.txt"
+
 
 @pytest.fixture
-def ransomware_test_data(data_for_tests_dir) -> Path:
-    return Path(data_for_tests_dir) / "ransomware_targets"
+def ransomware_test_data(data_for_tests_dir: Path) -> Path:
+    return data_for_tests_dir / "ransomware_targets"
 
 
 @pytest.fixture
@@ -15,3 +28,21 @@ def ransomware_target(tmp_path, ransomware_test_data) -> Path:
     shutil.copytree(ransomware_test_data, ransomware_target)
 
     return ransomware_target
+
+
+@pytest.fixture(params=[".m0nk3y", ".test", ""], ids=["monkeyext", "testext", "noext"])
+def ransomware_file_extension(request) -> str:
+    return request.param
+
+@pytest.fixture
+def home_env_variable():
+    if get_os() == OperatingSystem.WINDOWS:
+        return "%USERPROFILE%"
+    else:
+        return "$HOME"
+
+@pytest.fixture
+def patched_home_env(monkeypatch, tmp_path, home_env_variable):
+    monkeypatch.setenv(home_env_variable.strip("%$"), str(tmp_path))
+
+    return tmp_path
