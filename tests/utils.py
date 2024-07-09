1141439eb745e24d3dd2d97af463d326514d0837
Ilija Lazoroski <ilija.la@live.com>
2024-07-09 10:16:29 +0000

    Clear unused methods from tests.utils

--- tests/utils.py
+++ tests/utils.py
@@ -1,10 +1,6 @@
 import ctypes
-import filecmp
 import os
-import threading
 from pathlib import Path
-from typing import Iterable
-from unittest.mock import MagicMock
 
 from monkeytoolbox import get_binary_io_sha256_hash
 
@@ -16,33 +12,8 @@ def is_user_admin():
     return ctypes.windll.shell32.IsUserAnAdmin()
 
 
-def get_reference_to_exception_raising_function(ex):
-    def inner(ex):
-        raise_(ex)
-
-    return inner
-
-
 def raise_(ex):
     raise ex
-
-
-def add_subdirs_to_dir(parent_dir: Path, subdirs: Iterable[str]) -> Iterable[Path]:
-    subdir_paths = [parent_dir / s for s in subdirs]
-
-    for subdir in subdir_paths:
-        subdir.mkdir()
-
-    return subdir_paths
-
-
-def add_files_to_dir(parent_dir: Path, file_names: Iterable[str]) -> Iterable[Path]:
-    files = [parent_dir / f for f in file_names]
-
-    for f in files:
-        f.touch()
-
-    return files
 
 
 def get_file_sha256_hash(filepath: Path) -> str:
@@ -54,34 +25,3 @@ def get_file_sha256_hash(filepath: Path) -> str:
     """
     with open(filepath, "rb") as f:
         return get_binary_io_sha256_hash(f)
-
-
-def assert_directories_equal(dir1: Path, dir2: Path):
-    assert dir1.is_dir()
-    assert dir2.is_dir()
-
-    dircmp = filecmp.dircmp(dir1, dir2, ignore=[])
-
-    _assert_dircmp_equal(dircmp)
-
-
-def _assert_dircmp_equal(dircmp: filecmp.dircmp):
-    assert len(dircmp.diff_files) == 0
-    assert dircmp.left_list == dircmp.right_list
-
-    for subdir_cmp in dircmp.subdirs.values():
-        _assert_dircmp_equal(subdir_cmp)
-
-
-class ThreadSafeMagicMock:
-    def __init__(self, *args, **kwargs):
-        self._lock = threading.Lock()
-        self._mock = MagicMock()
-
-    def __call__(self, *args, **kwargs):
-        with self._lock:
-            return self._mock(*args, **kwargs)
-
-    def __getattr__(self, name):
-        with self._lock:
-            return getattr(self._mock, name)
